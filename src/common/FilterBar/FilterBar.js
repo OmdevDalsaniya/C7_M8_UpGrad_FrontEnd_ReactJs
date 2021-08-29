3e507fb7fdb9b27d67d14f6eb08973910ea988ef
Omiee123 <dalasaniya.169104144@muj.manipal.edu>
2021-08-29 19:18:23 +0530

    FilterBar Added, with theme

--- src/common/FilterBar/FilterBar.js
+++ src/common/FilterBar/FilterBar.js
@@ -1,11 +1,44 @@
 import React, { useState, useEffect } from "react";
+import { Card, CardContent, CardHeader, FormControl, InputLabel } from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
-const FilterBar = () => {
+const styles = (theme) => ({
+    heading: {
+        color: theme.palette.primary.light,
+    },
+    card: {
+        margin: theme.spacing.unit,
+    }
+});
+
+const FilterBar = (props) => {
+
+    const { classes } = props;
+
     return(
         <React.Fragment>
-            <p>FilterBar Page</p>
+            <Card
+                variant="outlined"
+                className={classes.card}
+            >
+                <CardContent>
+                <CardHeader title="FIND MOVIES BY:" className={classes.heading}/>
+                    <FormControl required className="formControl">
+                    <InputLabel 
+                        id="MovieName"
+                        label="Movie Name"
+                        name="movieName"
+                    />
+                    </FormControl>
+                </CardContent>                
+            </Card>
         </React.Fragment>
-    )
+    );
 }
 
-export default FilterBar;
+FilterBar.prototype = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(FilterBar);
--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../screens/home/Home";
 import BookShow from '../screens/bookshow/BookShow';
+import FilterBar from "../common/FilterBar/FilterBar";
 
 const Controller = () => {
 
@@ -9,7 +10,8 @@ const Controller = () => {
     return(
         <Router>
             <div>
-                <Route exact path="/" render={(props) => <Home {...props} /> } />
+                {/* <Route exact path="/" render={(props) => <Home {...props} /> } /> */}
+                <Route exact path="/" render={(props) => <FilterBar {...props} /> } />
                 <Route exact path="/BookShow" render={(props) => <BookShow {...props} />} />
                 <Route exact path="/Movie" render={(props) =><Home {...props}/>}/>
             </div>
